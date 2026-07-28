45b729616ec342620544c692b5c1ec9fd21a63dc
Revati Kamble <revati2855@gmail.com>
2026-07-28 18:44:41 +0530

    Add files via upload

---
+++ performance.py
@@ -0,0 +1,11 @@
+m = float(input("Enter the percentage:"))
+if m >= 90:
+    print("Excellent performance")
+elif m >= 80:
+    print("Very good performance")
+elif m >= 70:
+    print("Good performance")
+elif m >= 60:
+    print("Average performance")
+else:
+    print("Poor performance")
