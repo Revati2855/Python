637ecdd64fbf41cb473d6b30df0d3a07960899bc
Revati Kamble <revati2855@gmail.com>
2026-07-29 20:52:29 +0530

    Add files via upload

---
+++ largest_of_3.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the 1st num:"))
+b = int(input("Enter the 2nd num:"))
+c = int(input("Enter the 3rd num:"))
+if a>b and a>c:
+    print(a,"is greater")
+elif b>a and b>c:
+    print(b,"is greater")
+else:
+    print(c,"is greater")
---
+++ leap.py
@@ -0,0 +1,5 @@
+n = int(input("Enter the number:"))
+if n%4 ==  0:
+    print(n, "is leap year")
+else:
+    print(n,"is not leap year")
