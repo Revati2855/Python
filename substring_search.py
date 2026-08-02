1ee5800836c7e2c5709a7f3db517869bbbbe5ee4
Revati Kamble <revati2855@gmail.com>
2026-08-02 17:22:00 +0530

    Substring Search

---
+++ substring_search.py
@@ -0,0 +1,6 @@
+main_string = input("Enter a main string:")
+substring = input("Enter a substring :")
+if substring in main_string:
+    print("Substring exists.")
+else:
+    print("Substring does not exist.")
