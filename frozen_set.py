76ad8e9d1724b5c3b07e3b561b08699b42451a33
Revati Kamble <revati2855@gmail.com>
2026-08-22 07:12:49 -0700

    Frozen Set Operations

---
+++ frozen_set.py
@@ -0,0 +1,8 @@
+a = {1,2,3,4,5}
+b = {2,3,6,7,8}
+
+print("Union of a & b: ",a|b)
+print("Intersection of a & b: ",a&b)
+print("a-b : ",a-b)
+print("Symmetric diff : ",a^b)
+print("Is Subset : ",a<=b)
